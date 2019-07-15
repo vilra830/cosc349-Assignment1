4621f8f8352bc3eda34ca1a941e9d9e60702a38d
vilra830 <vilra830@SOB0486L.staff.uod.otago.ac.nz>
2019-07-16 09:59:27 +1200

    Shopping Project
    created shopping project and Administration class

---
+++ Administration.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vilra830
+ */
+public class Administration {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
