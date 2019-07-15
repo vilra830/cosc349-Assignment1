ceb3bf25d9173f811796532922294e540f18e479
vilra830 <vilra830@SOB0486L.staff.uod.otago.ac.nz>
2019-07-16 10:38:14 +1200

    Frame Design Complete
    Issue no. 2 Completed.
    Completed design of the frame at this point

---
+++ domain/Customer.java
@@ -0,0 +1,19 @@
+package domain;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vilra830
+ */
+public class Customer {
+    
+    private Integer customerID;
+    private String username;
+    private String firstName;
+    
+}
