6618e3840b374aa5f2518d079b64c7e32ecbbcae
vilra830 <vilra830@student.otago.ac.nz>
2019-08-13 20:44:22 +1200

    Created a Product Table
    Identified productID as primary Key
    copy pasted whole table in H2

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  villa
+ * Created: 13/08/2019
+ */
+   create table Product( 
+    productID varchar(20) ,
+    productName varchar(20) not null,
+    productDescription varchar(300) not null,
+    productCategory varchar(50) not null, 
+    stockQuantity decimal(5,2) not null,
+    priceList decimal(5,2) not null,
+
+    constraint Product_PK primary key (productID)
+);
+    
