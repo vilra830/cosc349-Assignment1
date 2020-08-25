d77191216af7be9d134522bb8310a6dcea8f48d9
Thomas Hunt <thomas.rob.hunt@gmail.com>
2020-08-25 15:05:46 +1200

    Added admin_side server to the Vagrant file, created admin sh for provison and admin.conf

---
+++ admin-interface.sh
@@ -0,0 +1,11 @@
+ apt-get update
+    apt-get install -y apache2 php libapache2-mod-php php-mysql
+
+    # Change VM's webserver's configuration to use shared folder.
+    # (Look inside user-interface.conf for specifics.)
+    cp /vagrant/admin-interface.conf /etc/apache2/sites-available/
+	
+    # install our website configuration and disable the default
+    a2ensite admin-interface
+    a2dissite 000-default
+    service apache2 reload
